4fbaaa3463dd2c5fc1e1b1886235a1eaed864c12
Stephen Jacobs <sjacobs@truefit.io>
2021-01-25 16:21:19 -0500

    fix meeting endpoint response

--- api/meeting.ts
+++ api/meeting.ts
@@ -2,5 +2,5 @@ import {NowRequest, NowResponse} from '@vercel/node';
 
 export default (request: NowRequest, response: NowResponse) => {
   console.log('ZOOM REQUEST: ', {...request.body});
-  return response.status(200).send({});
+  response.status(200).send({});
 };
