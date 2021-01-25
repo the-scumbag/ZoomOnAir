f629ea7deb46200283661769da9d28c91e9de919
Stephen Jacobs <sjacobs@truefit.io>
2021-01-25 16:36:34 -0500

    add logging for auth header

--- api/meeting.ts
+++ api/meeting.ts
@@ -3,6 +3,7 @@ import {NowRequest, NowResponse} from '@vercel/node';
 export default (request: NowRequest, response: NowResponse) => {
   console.log('ZOOM BODY:', {...request.body});
 
+  console.log('CHECKING AUTH: ', process.env.ZOOM_WEBHOOK_TOKEN);
   if (request.headers['authorization'] !== process.env.ZOOM_WEBHOOK_TOKEN) {
     console.log('AUTH ERROR: ', request.headers['authorization']);
     return response.status(401);
