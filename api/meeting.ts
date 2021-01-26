b0c948074a755a20e07e0c3eaabd482cdd35ebbb
Stephen Jacobs <sjacobs@truefit.io>
2021-01-25 21:18:32 -0500

    add wyze lib

--- api/meeting.ts
+++ api/meeting.ts
@@ -1,13 +1,32 @@
 import {NowRequest, NowResponse} from '@vercel/node';
+import Wyze from 'wyze-node';
 
-export default (request: NowRequest, response: NowResponse) => {
+const deviceTypes = ['Plug', 'Light'];
+const devicePrefix = 'zoom_';
+
+export default async (request: NowRequest, response: NowResponse) => {
   console.log('ZOOM BODY:', {...request.body});
 
-  console.log('CHECKING AUTH: ', process.env.ZOOM_WEBHOOK_TOKEN);
   if (request.headers['authorization'] !== process.env.ZOOM_WEBHOOK_TOKEN) {
-    console.log('AUTH ERROR: ', request.headers['authorization']);
     return response.status(401);
   }
+
+  const wyze = new Wyze({
+    username: process.env.WYZE_USERNAME,
+    password: process.env.WYZE_PASSWORD,
+  });
+
+  const devices = await wyze.getDeviceList();
+  const filtered = devices.filter(
+    (x) =>
+      deviceTypes.includes(x.product_type) &&
+      x.nickname.startsWith(devicePrefix),
+  );
+
+  console.log('DEVICES: ', filtered);
+
+  const promises = filtered.map((device) => wyze.turnOn(device));
+  await Promise.all(promises);
 
   response.status(200).send({});
 };
