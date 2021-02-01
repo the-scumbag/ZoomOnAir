11f0644014a6aa1fd42a8927cb62e56d3e572dd2
Stephen Jacobs <sjacobs@truefit.io>
2021-02-01 11:56:10 -0500

    cleanup logging

--- api/meeting.ts
+++ api/meeting.ts
@@ -12,25 +12,27 @@ export default async (request: NowRequest, response: NowResponse) => {
 
   const event = request.body.event;
   const participant = request.body.payload.object.participant;
+
   const isParticipantEvent =
     event &&
     participant &&
     (event === participantJoinedEvent || event === participantLeftEvent);
 
-  console.log({isParticipantEvent});
   if (!authValid || !isParticipantEvent) {
     // just return a 200, we don't want zoom to keep retrying for a non-2xx error;
     return response.status(200).send({});
   }
 
+  console.log('Zoom authenticated...');
+  console.log('Event parse successful...');
+
   const isValidParticipant = participant.email === process.env.ZOOM_PARTICIPANT;
-  console.log({participant});
-  console.log({isValidParticipant});
   if (!isValidParticipant) {
     return response.status(200).send({});
   }
 
-  console.log('connecting to wyze...');
+  console.log('Zoom participant verified...');
+  console.log('Connecting to Wyze...');
 
   const options = {
     username: process.env.WYZE_USERNAME,
